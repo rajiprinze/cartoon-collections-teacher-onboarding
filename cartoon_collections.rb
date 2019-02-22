de137b2e01832a31122949af32e511986ae4f38a
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-22 01:30:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(arr)
 end
 
 def long_planeteer_calls(arr)# code an argument here
-  arr.select |e| 
+  arr.select |e| e.length > 4
 end
 
 def find_the_cheese# code an argument here
