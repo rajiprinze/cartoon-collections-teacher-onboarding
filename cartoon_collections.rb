6831e41c0c7163b5461ba3f450e800f2775f4c95
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-22 01:29:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(arr)# code an argument here
 end
 
 def summon_captain_planet(arr)
-  arr.map |xx| xx.upcase + "!"
+  arr.map |el| el.upcase + "!"
 end
 
 def long_planeteer_calls(arr)# code an argument here
