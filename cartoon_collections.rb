81387118515c2a3b0b6e015c0206936590b9198d
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-22 01:27:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(arr)# code an argument here
   }
 end
 
-def summon_captain_planet(array)# code an argument here
-  array.map |element| x.upcase + "!"
+def summon_captain_planet(array)
+  
 end
 
 def long_planeteer_calls(array)# code an argument here
